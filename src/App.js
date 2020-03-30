f326121a26bba91c3c985e6a2606e7808be11895
Zach Jones <Zach.Jones@Benjaminmoore.com>
2020-03-30 15:45:12 -0500

    added person component. Learned about props and props children

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person name="Tracer" age="26" />
+        <Person name="Junkrat" age="25">My Hobbies: Blowing stuff up</Person>
+        <Person name="Mei" age="31"/>
       </div>
     );
+    //The code above compiles down to the line below this. Neat!
+    //return React.createElement('div', null, React.createElement('h1', {className:'App'}, 'Hi, I\'m a React App'))
   }
 }
 
