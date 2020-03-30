bfc4fe4736d05b0cc9ce1cbfa86d590763fc9eab
Zach Jones <Zach.Jones@Benjaminmoore.com>
2020-03-30 16:38:15 -0500

    learned about state and setting state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Tracer', age: 26 }, 
+      { name: 'Junkrat', age: 31 }, 
+      { name: 'Mei', age: 25 } 
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log('was clicked')
+    //DON'T DO THIS: this.state.persons[0].name = "Super Tracer"
+    this.setState({
+      persons: [
+        { name: 'Lena', age: 26 }, 
+        { name: 'Junkrat', age: 25 }, 
+        { name: 'Mei', age: 41 } 
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="Tracer" age="26" />
-        <Person name="Junkrat" age="25">My Hobbies: Blowing stuff up</Person>
-        <Person name="Mei" age="31"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: splodin' stuff</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     //The code above compiles down to the line below this. Neat!
